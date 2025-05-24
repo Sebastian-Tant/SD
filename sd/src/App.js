0e6b83708e5dccac855ecb3aa46e3bb028cfb1d7
hadibutt3560 <hadibutt3560@gmail.com>
2025-05-24 17:43:47 +0200

    landing page ui border fix

--- sd/src/App.js
+++ sd/src/App.js
@@ -1,11 +1,9 @@
 // src/App.js
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify'; // Add this import
-import 'react-toastify/dist/ReactToastify.css'; // Add this import
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Explore from './components/Explore';
+import Explore from './components/Explore'; // Import the new Explore page
 import Applications from './components/Applications';
 import AdminDashboard from './components/AdminDashboard';
 import Footer from './components/Footer';
@@ -13,12 +11,13 @@ import './App.css';
 import AddFacility from "./components/AddFacility";
 import BookFacility from './components/BookFacility';
 import Analytics from './components/Analytics';
+
 import BookingPage from './components/BookingPage';
 import ReportsPage from './components/ReportsPage';
 import Features from './components/Features';
 import Events from './components/Events';
 import EventForm from './components/EventForm';
-import MyBookings from './components/MyBookings';
+import MyBookings from './components/MyBookings'; // <--- NEW IMPORT
 import ApplicationStatus from './components/ApplicationStatus'; 
 
 function ScrollToTopButton() {
@@ -50,7 +49,7 @@ function ScrollToTopButton() {
 function App() {
   return (
     <Router>
-      <section className="App">
+      <div className="App">
         <Navbar />
         <main className="main-content">
           <Routes>
@@ -58,8 +57,8 @@ function App() {
             <Route path="/" element={
               <>
                 <Hero />
-                <Features />
-                <Applications/>
+               < Features />
+               <Applications/>
               </>
             } />
             <Route path="/explore" element={<Explore />} />
@@ -67,10 +66,9 @@ function App() {
             <Route path="/add-facility" element={<AddFacility />} />
             <Route path="/facility/:id/book" element={<BookFacility />} />
             <Route path="/analytics" element={<Analytics />} />
-            <Route path="/my-bookings" element={<MyBookings />} />
+            <Route path="/my-bookings" element={<MyBookings />} /> {/* NEW ROUTE */}
             <Route path="/applications" element={<Applications />} />
-            <Route path="/application-status" element={<ApplicationStatus />} />
-            <Route path="/bookings" element={<BookingPage />} />
+            <Route path="/application-status" element={<ApplicationStatus />} />            <Route path="/bookings" element={<BookingPage />} />
             <Route path="/reports" element={<ReportsPage/>} />
             <Route path="/events" element={<Events />} />
             <Route path="/add-event" element={<EventForm />} />
@@ -78,20 +76,7 @@ function App() {
         </main>
         <Footer />
         <ScrollToTopButton />
-        {/* Add ToastContainer here */}
-        <ToastContainer 
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        />
-      </section>
+      </div>
     </Router>
   );
 }
